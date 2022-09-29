4207ab3f6ffd95555dfc31704a8a288c774cd730
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-09-29 09:04:02 -0300

    atualizaçao

--- eventos.js
+++ eventos.js
@@ -2,40 +2,37 @@
     // fetch()//aqui é realizada a busca
     //    .then()//quando a resposta chega, a gente extrai o json dela
     //    .then();//aqui a gente usa o jason extraido.
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
-    .then((resposta)=> resposta.json())
-    .then((regioes)=>{
-        regioes.map((cadaRegiao)=>{
-            document.getElementById('regiao').innerHTML +=`
-            <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
+fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes`)
+.then((resposta)=> resposta.json())
+.then((regioes)=>{
+    regioes.map((cadaRegiao)=>{
+        document.getElementById('regiao').innerHTML +=`
+        <option value="${cadaRegiao.id}">${cadaRegiao.nome}</option>
+        `;
+    });
+});
+function mostrarEstado(){
+        let regiaoSelecionada = regiao.value;
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${regiaoSelecionada}/estados`)
+        .then((resposta)=> resposta.json())
+        .then((estados)=>{
+            estados.map((cadaEstado)=>{
+                document.getElementById('estado').innerHTML +=`
+                <option value="${cadaEstado.id}">${cadaEstado.nome}</option>
             `;
         });
     });
-    
-function selecionarRegiao(){
-        let estadoSelecionado = regiao.value;
-        // alert(estadoSelecionado);
-
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${estadoSelecionado}/estados`)
-    .then((resposta)=> resposta.json())
-    .then((estados)=>{
-        // console.log(estados);
-        estados.map((cadaEstado)=>{
-            document.getElementById('estado').innerHTML +=`
-            <option>${cadaEstado.nome}</option>
-            `;
-    });
-});
 }
-
-
-
-// fetch(`https://servicodados.ibge.gov.br/api/v1/localidades`)
-// .then((resposta)=> resposta.json())
-// .then((cidades)=>{
-//     cidades.map((cadaCidade)=>{
-//         document.getElementById('cidade').innerHTML +=`
-//         <option>${cadaCidade.nome}</option>
-//         `;
-//     });
-// });
+function mostrarCidade(){
+        let estadoSelecionado = estado.value;
+        fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${estadoSelecionado}/distritos`)
+        .then( resposta => resposta.json())
+        .then((cidades)=>{
+            console.log(cidades);
+            cidades.map((cadaCidade)=>{
+                document.getElementById('cidade').innerHTML +=`
+                <option value="${cadaCidade.id}">${cadaCidade.nome}</option>
+            `;
+        });
+    });
+}
